4127f45849e633bca337d1c780c00f0dfed86ab2
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-22 13:44:09 -0500

    Updating code for online repo

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,6 +5,8 @@ function Resume() {
     return (
         <div>
             <h1>Resume</h1>
+
+            <a href='https://docs.google.com/document/d/1mXOh1Eng49DR69Uh20_O-cTwm76QDZ9M-LXohsDHGeY/edit?usp=sharing'>Google Docs Resume Link</a>
         </div>
     )
 }
