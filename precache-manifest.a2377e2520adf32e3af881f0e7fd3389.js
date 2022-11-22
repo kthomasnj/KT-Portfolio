9678c5401421839003387f83d25168c6db2f4854
Keith S Thomas <kthomasnj@hotmail.com>
2022-11-22 10:47:28 -0500

    Deploy React app to GitHub Pages

---
+++ precache-manifest.a2377e2520adf32e3af881f0e7fd3389.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f99eec88be06d6230601",
+    "url": "/Keith-Thomas-DEV-Portfolio/static/css/main.6ea38ad7.chunk.css"
+  },
+  {
+    "revision": "f99eec88be06d6230601",
+    "url": "/Keith-Thomas-DEV-Portfolio/static/js/main.f99eec88.chunk.js"
+  },
+  {
+    "revision": "4d5909f16dbfde8279fc",
+    "url": "/Keith-Thomas-DEV-Portfolio/static/js/1.4d5909f1.chunk.js"
+  },
+  {
+    "revision": "984538b944331207f588",
+    "url": "/Keith-Thomas-DEV-Portfolio/static/js/2.984538b9.chunk.js"
+  },
+  {
+    "revision": "022ad9d9e74913f784cb",
+    "url": "/Keith-Thomas-DEV-Portfolio/static/js/runtime~main.022ad9d9.js"
+  },
+  {
+    "revision": "4db78858a38fcc3e0f90a583b29fcf7b",
+    "url": "/Keith-Thomas-DEV-Portfolio/index.html"
+  }
+];
